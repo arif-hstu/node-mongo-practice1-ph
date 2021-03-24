9e30810c66ef3a4309edb704c7cab7e2909b8936
arif-hstu <arif.dvm.hstu@gmail.com>
2021-03-24 22:19:22 +0600

    create and read of crud applied

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+// require mongoClient
+const MongoClient = require('mongodb').MongoClient;
+
+const password = 'pCWt1AYUBfqguj0W';
+const uri = `mongodb+srv://arif-hstu:${password}@cluster0.rqnu2.mongodb.net/organicDb?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-app.listen(3000, 'localhost')
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.listen(3004, 'localhost')
 
 app.get('/', (req, res) => {
-    res.send('how are you?')
+    res.sendFile(__dirname + '/index.html');
 })
+
+
+client.connect(err => {
+    const productCollection = client.db("organicDb").collection("products");
+
+    // get data from database
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+    // perform actions on the collection object
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        productCollection.insertOne(product)
+            .then(result => {
+                console.log('added succesfully')
+                res.send('data added successfully')
+            })
+    })
+
+
+
+});
